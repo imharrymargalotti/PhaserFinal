836218699b2b3483f912fd8729b6a5660266991a
harry margalotti <imharrymargalotti@gmail.com>
2017-12-18 02:49:37 -0500

    final changes with sounds and readme

---
+++ README.txt
@@ -0,0 +1,20 @@
+@EliWennbergSmith @HarryMargalotti
+12/17/17
+
+_______________________
+Description:
+
+Welcome to FEEL THE BERN! In this game you will play as Bern Sandwiches. The object of the game is to infiltrate the Drumpf House where you will eventually face the evil dictator Drumpf himself. In order to defeat him, you must use your trusty squirt gun to eliminate his terrible spray tan. GOOD LUCK
+
+_______________________
+Controls:
+
+Move Left: left key
+Move Right: right key
+Jump: up key
+Shoot: SpaceBar
+
+_______________________
+About:
+
+This game was developed with the JavaScript library phaser. It utilizes basic physics, shooting mechanics, and artificial intelligence. 
--- phaser-ce-2.9.2/build/level1.js
+++ phaser-ce-2.9.2/build/level1.js
@@ -16,6 +16,7 @@ var weapon;
 var blocks;
 var level2background;
 var level3background;
+var gunsound;
 
 var trump1;
 
@@ -38,6 +39,8 @@ var enemy15;
 var enemy16;
 var enemy17;
 var enemy18;
+var enemy19;
+var hurt;
 
 function preload() {
 
@@ -53,6 +56,9 @@ function preload() {
     game.load.image('level3',"assets/images/level3.png");
     game.load.spritesheet('enemy',"assets/images/enemy.png",70,125);
     game.load.spritesheet('trumpEnemy',"assets/images/trump.png",90,200);
+    game.load.audio('gun',"assets/audio/Spit_Splat-Mike_Koenig-1170500447.mp3");
+    game.load.audio('music',"assets/audio/Darude - Sandstorm.mp3" );
+    game.load.audio('hurt',"assets/audio/Pain-SoundBible.com-1883168362.mp3");
     game.stage.backgroundColor = 'background';
 }
 
@@ -103,8 +109,12 @@ function Trump(index,game,x,y) {
 
 
 function hitEnemy (enemy,bullet) {
-    enemy.kill();
+    hurt.play();
     // bullet.kill();
+    enemy.kill();
+    bullet.x=-100;
+    bullet.y=0;
+
     enemy.x=-100;
     enemy.y=0;
     console.log("Hit");
@@ -116,6 +126,11 @@ function create() {
     level3background = game.add.tileSprite(20000,0,3000,600,'level3');
     player = game.add.sprite(200, 200, 'player',4);
     player.scale.setTo(1.5,1.5);
+
+    var music = game.add.audio('music');
+
+
+    music.play();
 
 
     //TRUMP
@@ -142,13 +157,33 @@ function create() {
     //end level 1
     enemy10 = new Enemy(7,game,10500, 500);
 
+    enemy11 = new Enemy(8,game,11500, 500);
+
+    enemy12 = new Enemy(9,game,12500, 500);
+
+    enemy13 = new Enemy(10,game,13500, 500);
+
+    enemy14 = new Enemy(11,game,14500, 500);
+
+    enemy15 = new Enemy(12,game,15500, 500);
+
+    enemy16 = new Enemy(13,game,16500, 500);
+
+    enemy17 = new Enemy(14,game,17500, 500);
+
+    enemy18 = new Enemy(15,game,18500, 500);
+
+    enemy19 = new Enemy(16,game,19500, 500);
+
 
 
     weapon = game.add.weapon(100,'bullet');
-    weapon.bulletKillType = Phaser.Weapon.KILL_CAMERA_BOUNDS;
     weapon.bulletSpeed = 1000;
     weapon.trackSprite(player, 80,125);
-    weapon.fireRate = 100;
+    weapon.fireRate = 1000;
+
+    weapon.bulletKillType = Phaser.Weapon.KILL_CAMERA_BOUNDS;
+
 
     game.physics.arcade.enable(player);
     player.setHealth(300);
@@ -165,6 +200,9 @@ function create() {
 
     game.world.setBounds(0,0,23000,600);
 
+    gunsound = game.add.audio('gun');
+    hurt = game.add.audio("hurt");
+
     player.body.collideWorldBounds = true;
     player.body.gravity.y = 1000;
 
@@ -177,14 +215,24 @@ function create() {
 
     obsticle1();
 
-}function update () {
+}
+function killPlayer() {
+    player.kill();
+    //bmpText = game.add.bitmapText(10, 100, 'carrier_command','YOU LOSE',34);
+    // bmpText.inputEnabled = true;
+    // bmpText.input.enableDrag();
+}
+
+function update () {
     this.myHealthBar.setPercent(player.health);
     this.trumpBar.setPercent(trump1.health);
     this.trumpBar.setPosition(trump1.x,200);
     game.physics.arcade.collide(player, blocks);
     game.physics.arcade.collide(bullet, blocks);
 
-
+    if(player.health<0){
+        killPlayer();
+    }
     player.body.velocity.x = 0;
 
     if (cursors.left.isDown)
@@ -219,8 +267,10 @@ function create() {
     if(shoot.isDown){
         if (player.frame == 3 || player.frame == 2 || player.frame == 1) {
             weapon.fireAngle = Phaser.ANGLE_LEFT;
+            gunsound.play();
         } else if (player.frame == 5 || player.frame == 6 || player.frame == 7) {
             weapon.fireAngle = Phaser.ANGLE_RIGHT;
+            gunsound.play();
         }
         weapon.fire();
     }
@@ -561,28 +611,414 @@ function create() {
         enemy9.animations.play("walkBack");
     }
 
+    //enemy9END-----------------------------------------------------------------------------------------------------------
+    game.physics.arcade.collide(enemy10, blocks);
+    game.physics.arcade.collide(enemy10, player);
+    if(game.physics.arcade.collide(enemy10, weapon.bullets)){
+        hitEnemy(enemy10,weapon.bullets);
+    }//this works
+    //game.physics.arcade.overlap(bullet, enemy1);
+    if((enemy10.x<=player.x+200 && enemy10.x>=player.x-200 || enemy10.x>= player.x-200 && enemy10.x<=player.x+200)){
+        if(enemy10.body.velocity.x>0){
+            //enemy1.body.velocity.x=0;
+            enemy10.animations.play('punchForwards');
+            //enemy1.frame=8;
+            player.health-=1;
+        }
+        else if(enemy10.body.velocity.x<0){
+            // enemy3.body.velocity.x=0;
+            enemy10.animations.play('punchBack');
+            //enemy1.frame=0;
+            player.health-=1;
+        }
+    }
+    if (enemy10.x < player.x + 500){
+        enemy10.body.velocity.x=-225;
+    }
+    if(enemy10.x <= player.x){
+        enemy10.body.velocity.x*=-1;
+
+    }
+    if(enemy10.body.velocity.x==0){
+        enemy10.frame=4;
+    }
+    else if(enemy10.body.velocity.x>0){
+        enemy10.animations.play("walk");
+    }
+    else{
+        enemy10.animations.play("walkBack");
+    }
+
+    //enemy10END-----------------------------------------------------------------------------------------------------------
+    game.physics.arcade.collide(enemy11, blocks);
+    game.physics.arcade.collide(enemy11, player);
+    if(game.physics.arcade.collide(enemy11, weapon.bullets)){
+        hitEnemy(enemy11,weapon.bullets);
+    }//this works
+    //game.physics.arcade.overlap(bullet, enemy1);
+    if((enemy11.x<=player.x+200 && enemy11.x>=player.x-200 || enemy11.x>= player.x-200 && enemy11.x<=player.x+200)){
+        if(enemy11.body.velocity.x>0){
+            //enemy1.body.velocity.x=0;
+            enemy11.animations.play('punchForwards');
+            //enemy1.frame=8;
+            player.health-=1;
+        }
+        else if(enemy11.body.velocity.x<0){
+            // enemy3.body.velocity.x=0;
+            enemy11.animations.play('punchBack');
+            //enemy1.frame=0;
+            player.health-=1;
+        }
+    }
+    if (enemy11.x < player.x + 500){
+        enemy11.body.velocity.x=-225;
+    }
+    if(enemy11.x <= player.x){
+        enemy11.body.velocity.x*=-1;
+
+    }
+    if(enemy11.body.velocity.x==0){
+        enemy11.frame=4;
+    }
+    else if(enemy11.body.velocity.x>0){
+        enemy11.animations.play("walk");
+    }
+    else{
+        enemy11.animations.play("walkBack");
+    }
+
+    //enemy11END-----------------------------------------------------------------------------------------------------------
+    game.physics.arcade.collide(enemy12, blocks);
+    game.physics.arcade.collide(enemy12, player);
+    if(game.physics.arcade.collide(enemy12, weapon.bullets)){
+        hitEnemy(enemy12,weapon.bullets);
+    }//this works
+    //game.physics.arcade.overlap(bullet, enemy1);
+    if((enemy12.x<=player.x+200 && enemy12.x>=player.x-200 || enemy12.x>= player.x-200 && enemy12.x<=player.x+200)){
+        if(enemy12.body.velocity.x>0){
+            //enemy1.body.velocity.x=0;
+            enemy12.animations.play('punchForwards');
+            //enemy1.frame=8;
+            player.health-=1;
+        }
+        else if(enemy12.body.velocity.x<0){
+            // enemy3.body.velocity.x=0;
+            enemy12.animations.play('punchBack');
+            //enemy1.frame=0;
+            player.health-=1;
+        }
+    }
+    if (enemy12.x < player.x + 500){
+        enemy12.body.velocity.x=-225;
+    }
+    if(enemy12.x <= player.x){
+        enemy12.body.velocity.x*=-1;
+
+    }
+    if(enemy12.body.velocity.x==0){
+        enemy12.frame=4;
+    }
+    else if(enemy12.body.velocity.x>0){
+        enemy12.animations.play("walk");
+    }
+    else{
+        enemy12.animations.play("walkBack");
+    }
+
+    //enemy12END-----------------------------------------------------------------------------------------------------------
+    game.physics.arcade.collide(enemy13, blocks);
+    game.physics.arcade.collide(enemy13, player);
+    if(game.physics.arcade.collide(enemy13, weapon.bullets)){
+        hitEnemy(enemy13,weapon.bullets);
+    }//this works
+    //game.physics.arcade.overlap(bullet, enemy1);
+    if((enemy13.x<=player.x+200 && enemy13.x>=player.x-200 || enemy13.x>= player.x-200 && enemy13.x<=player.x+200)){
+        if(enemy13.body.velocity.x>0){
+            //enemy1.body.velocity.x=0;
+            enemy13.animations.play('punchForwards');
+            //enemy1.frame=8;
+            player.health-=1;
+        }
+        else if(enemy13.body.velocity.x<0){
+            // enemy3.body.velocity.x=0;
+            enemy13.animations.play('punchBack');
+            //enemy1.frame=0;
+            player.health-=1;
+        }
+    }
+    if (enemy13.x < player.x + 500){
+        enemy12.body.velocity.x=-225;
+    }
+    if(enemy13.x <= player.x){
+        enemy13.body.velocity.x*=-1;
+
+    }
+    if(enemy13.body.velocity.x==0){
+        enemy13.frame=4;
+    }
+    else if(enemy13.body.velocity.x>0){
+        enemy13.animations.play("walk");
+    }
+    else{
+        enemy13.animations.play("walkBack");
+    }
+
+    //enemy13END-----------------------------------------------------------------------------------------------------------
+    game.physics.arcade.collide(enemy14, blocks);
+    game.physics.arcade.collide(enemy14, player);
+    if(game.physics.arcade.collide(enemy14, weapon.bullets)){
+        hitEnemy(enemy14,weapon.bullets);
+    }//this works
+    //game.physics.arcade.overlap(bullet, enemy1);
+    if((enemy14.x<=player.x+200 && enemy14.x>=player.x-200 || enemy14.x>= player.x-200 && enemy14.x<=player.x+200)){
+        if(enemy14.body.velocity.x>0){
+            //enemy1.body.velocity.x=0;
+            enemy14.animations.play('punchForwards');
+            //enemy1.frame=8;
+            player.health-=1;
+        }
+        else if(enemy14.body.velocity.x<0){
+            // enemy3.body.velocity.x=0;
+            enemy14.animations.play('punchBack');
+            //enemy1.frame=0;
+            player.health-=1;
+        }
+    }
+    if (enemy14.x < player.x + 500){
+        enemy14.body.velocity.x=-225;
+    }
+    if(enemy14.x <= player.x){
+        enemy14.body.velocity.x*=-1;
+
+    }
+    if(enemy14.body.velocity.x==0){
+        enemy14.frame=4;
+    }
+    else if(enemy14.body.velocity.x>0){
+        enemy14.animations.play("walk");
+    }
+    else{
+        enemy14.animations.play("walkBack");
+    }
+
+    //enemy14END-----------------------------------------------------------------------------------------------------------
+    game.physics.arcade.collide(enemy15, blocks);
+    game.physics.arcade.collide(enemy15, player);
+    if(game.physics.arcade.collide(enemy15, weapon.bullets)){
+        hitEnemy(enemy15,weapon.bullets);
+    }//this works
+    //game.physics.arcade.overlap(bullet, enemy1);
+    if((enemy15.x<=player.x+200 && enemy15.x>=player.x-200 || enemy15.x>= player.x-200 && enemy15.x<=player.x+200)){
+        if(enemy15.body.velocity.x>0){
+            //enemy1.body.velocity.x=0;
+            enemy15.animations.play('punchForwards');
+            //enemy1.frame=8;
+            player.health-=1;
+        }
+        else if(enemy15.body.velocity.x<0){
+            // enemy3.body.velocity.x=0;
+            enemy15.animations.play('punchBack');
+            //enemy1.frame=0;
+            player.health-=1;
+        }
+    }
+    if (enemy15.x < player.x + 500){
+        enemy15.body.velocity.x=-225;
+    }
+    if(enemy15.x <= player.x){
+        enemy15.body.velocity.x*=-1;
+
+    }
+    if(enemy15.body.velocity.x==0){
+        enemy15.frame=4;
+    }
+    else if(enemy15.body.velocity.x>0){
+        enemy15.animations.play("walk");
+    }
+    else{
+        enemy15.animations.play("walkBack");
+    }
+
+    //enemy15END-----------------------------------------------------------------------------------------------------------
+    game.physics.arcade.collide(enemy16, blocks);
+    game.physics.arcade.collide(enemy16, player);
+    if(game.physics.arcade.collide(enemy16, weapon.bullets)){
+        hitEnemy(enemy16,weapon.bullets);
+    }//this works
+    //game.physics.arcade.overlap(bullet, enemy1);
+    if((enemy16.x<=player.x+200 && enemy16.x>=player.x-200 || enemy16.x>= player.x-200 && enemy16.x<=player.x+200)){
+        if(enemy16.body.velocity.x>0){
+            //enemy1.body.velocity.x=0;
+            enemy16.animations.play('punchForwards');
+            //enemy1.frame=8;
+            player.health-=1;
+        }
+        else if(enemy16.body.velocity.x<0){
+            // enemy3.body.velocity.x=0;
+            enemy16.animations.play('punchBack');
+            //enemy1.frame=0;
+            player.health-=1;
+        }
+    }
+    if (enemy16.x < player.x + 500){
+        enemy16.body.velocity.x=-225;
+    }
+    if(enemy16.x <= player.x){
+        enemy16.body.velocity.x*=-1;
+
+    }
+    if(enemy16.body.velocity.x==0){
+        enemy16.frame=4;
+    }
+    else if(enemy16.body.velocity.x>0){
+        enemy16.animations.play("walk");
+    }
+    else{
+        enemy16.animations.play("walkBack");
+    }
+
+    //enemy16END-----------------------------------------------------------------------------------------------------------
+    game.physics.arcade.collide(enemy17, blocks);
+    game.physics.arcade.collide(enemy17, player);
+    if(game.physics.arcade.collide(enemy17, weapon.bullets)){
+        hitEnemy(enemy17,weapon.bullets);
+    }//this works
+    //game.physics.arcade.overlap(bullet, enemy1);
+    if((enemy17.x<=player.x+200 && enemy17.x>=player.x-200 || enemy17.x>= player.x-200 && enemy17.x<=player.x+200)){
+        if(enemy17.body.velocity.x>0){
+            //enemy1.body.velocity.x=0;
+            enemy17.animations.play('punchForwards');
+            //enemy1.frame=8;
+            player.health-=1;
+        }
+        else if(enemy17.body.velocity.x<0){
+            // enemy3.body.velocity.x=0;
+            enemy17.animations.play('punchBack');
+            //enemy1.frame=0;
+            player.health-=1;
+        }
+    }
+    if (enemy17.x < player.x + 500){
+        enemy17.body.velocity.x=-225;
+    }
+    if(enemy17.x <= player.x){
+        enemy17.body.velocity.x*=-1;
+
+    }
+    if(enemy17.body.velocity.x==0){
+        enemy17.frame=4;
+    }
+    else if(enemy17.body.velocity.x>0){
+        enemy17.animations.play("walk");
+    }
+    else{
+        enemy17.animations.play("walkBack");
+    }
+
+    //enemy17END-----------------------------------------------------------------------------------------------------------
+    game.physics.arcade.collide(enemy18, blocks);
+    game.physics.arcade.collide(enemy18, player);
+    if(game.physics.arcade.collide(enemy18, weapon.bullets)){
+        hitEnemy(enemy18,weapon.bullets);
+    }//this works
+    //game.physics.arcade.overlap(bullet, enemy1);
+    if((enemy18.x<=player.x+200 && enemy18.x>=player.x-200 || enemy18.x>= player.x-200 && enemy18.x<=player.x+200)){
+        if(enemy18.body.velocity.x>0){
+            //enemy1.body.velocity.x=0;
+            enemy18.animations.play('punchForwards');
+            //enemy1.frame=8;
+            player.health-=1;
+        }
+        else if(enemy18.body.velocity.x<0){
+            // enemy3.body.velocity.x=0;
+            enemy18.animations.play('punchBack');
+            //enemy1.frame=0;
+            player.health-=1;
+        }
+    }
+    if (enemy18.x < player.x + 500){
+        enemy18.body.velocity.x=-225;
+    }
+    if(enemy18.x <= player.x){
+        enemy18.body.velocity.x*=-1;
+
+    }
+    if(enemy18.body.velocity.x==0){
+        enemy18.frame=4;
+    }
+    else if(enemy18.body.velocity.x>0){
+        enemy18.animations.play("walk");
+    }
+    else{
+        enemy18.animations.play("walkBack");
+    }
+
+    //enemy18END-----------------------------------------------------------------------------------------------------------
+    game.physics.arcade.collide(enemy19, blocks);
+    game.physics.arcade.collide(enemy19, player);
+    if(game.physics.arcade.collide(enemy19, weapon.bullets)){
+        hitEnemy(enemy19,weapon.bullets);
+    }//this works
+    //game.physics.arcade.overlap(bullet, enemy1);
+    if((enemy19.x<=player.x+200 && enemy19.x>=player.x-200 || enemy19.x>= player.x-200 && enemy19.x<=player.x+200)){
+        if(enemy19.body.velocity.x>0){
+            //enemy1.body.velocity.x=0;
+            enemy19.animations.play('punchForwards');
+            //enemy1.frame=8;
+            player.health-=1;
+        }
+        else if(enemy19.body.velocity.x<0){
+            // enemy3.body.velocity.x=0;
+            enemy19.animations.play('punchBack');
+            //enemy1.frame=0;
+            player.health-=1;
+        }
+    }
+    if (enemy19.x < player.x + 500){
+        enemy19.body.velocity.x=-225;
+    }
+    if(enemy19.x <= player.x){
+        enemy19.body.velocity.x*=-1;
+
+    }
+    if(enemy19.body.velocity.x==0){
+        enemy19.frame=4;
+    }
+    else if(enemy19.body.velocity.x>0){
+        enemy19.animations.play("walk");
+    }
+    else{
+        enemy19.animations.play("walkBack");
+    }
+
     //TRUMP1--------------------------------------------------------------------------------------------------------------
+    //enemy18END-----------------------------------------------------------------------------------------------------------
     game.physics.arcade.collide(trump1, blocks);
     game.physics.arcade.collide(trump1, player);
-    game.physics.arcade.collide(trump1, weapon.bullet);
+    if(game.physics.arcade.collide(trump1, weapon.bullets)){
+        hitTrump(trump1);
 
-    if((trump1.x<=player.x+50 && trump1.x>=player.x-50 || trump1.x>= player.x-50 && trump1.x<=player.x+50)&&player.y<145){
+    }//this works
+    //game.physics.arcade.overlap(bullet, enemy1);
+    if((trump1.x<=player.x+200 && trump1.x>=player.x-200 || trump1.x>= player.x-200 && trump1.x<=player.x+200)){
         if(trump1.body.velocity.x>0){
             //enemy1.body.velocity.x=0;
             trump1.animations.play('punchForwards');
-            player.health-=25;
+            //enemy1.frame=8;
+            player.health-=15;
         }
         else if(trump1.body.velocity.x<0){
-            // enemy1.body.velocity.x=0;
+            // enemy3.body.velocity.x=0;
             trump1.animations.play('punchBack');
-            player.health-=25;
+            //enemy1.frame=0;
+            player.health-=15;
         }
     }
     if (trump1.x < player.x + 500){
         trump1.body.velocity.x=-225;
     }
     if(trump1.x <= player.x){
-        trump1.body.velocity.x*=-1;
+        trump1.body.velocity.x*=-15;
 
     }
     if(trump1.body.velocity.x==0){
@@ -594,6 +1030,15 @@ function create() {
     else{
         trump1.animations.play("walkBack");
     }
-}function render () {
+
+}
+function hitTrump(enemy,bullet) {
+    trump1.health-=5;
+    if(trump1.health<=0){
+        trump1.kill();
+        trump1.x=-100;
+    }
+}
+function render () {
     weapon.debug();
 }
